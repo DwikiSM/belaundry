0ee330c4f722ef2a42d91695d45dafe4186faf42
Dwiki <dwikism@gmail.com>
2024-11-07 13:42:12 +0700

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,26 @@ import axios from 'axios'
 const BASE_URL = import.meta.env.VITE_HOST
 const KEY = import.meta.env.VITE_KEY
 
+export const getProduct = async (id) =>
+  await axios.get(BASE_URL + '/platform/product/' + id, {
+    headers: { token: KEY },
+  })
+
+export const getProducts = async () =>
+  await axios.get(BASE_URL + '/platform/product', {
+    headers: { token: KEY },
+  })
+
+export const getProductsReport = async () =>
+  await axios.get(BASE_URL + '/platform/product/report', {
+    headers: { token: KEY },
+  })
+
+export const getProductsCategory = async () =>
+  await axios.get(BASE_URL + '/platform/product/categories', {
+    headers: { token: KEY },
+  })
+
 export const getUserInfo = async () =>
   await axios.get(BASE_URL + '/platform/user/info', {
     headers: { token: KEY },
